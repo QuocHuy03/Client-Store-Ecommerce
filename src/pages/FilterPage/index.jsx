e3217243b77dbb73729aacff9b49b449a60f3781
huy <huy@gmail.com>
2023-07-25 13:03:33 +0700

    huydev

--- src/pages/FilterPage/index.jsx
+++ src/pages/FilterPage/index.jsx
@@ -96,8 +96,6 @@ const FilterPage = () => {
     const query = Object.entries(filters)
       .map(([key, value]) => {
         if (Array.isArray(value)) {
-          // For array filters (e.g., categories), check if the array has any selected values.
-          // If yes, only take the first selected value (since radio buttons can only have one selection).
           if (value.length > 0) {
             return `${key}=${encodeURIComponent(value[0])}`;
           }
@@ -121,31 +119,32 @@ const FilterPage = () => {
     }));
   };
 
-  // let filteredData = data;
-  // if (data && data.length > 0) {
-  //   filteredData = data.filter((job) => {
-  //     if (filters.jobType && filters.jobType !== job.type_job) {
-  //       return false;
-  //     }
-  //     if (filters.jobRoles && filters.jobRoles !== job.level) {
-  //       return false;
-  //     }
-  //     if (filters.salaryRange) {
-  //       const [minSalary, maxSalary] = filters.salaryRange.split("-");
-  //       const jobSalary = parseInt(job.salary);
-  //       if (minSalary && jobSalary < parseInt(minSalary)) {
-  //         return false;
-  //       }
-  //       if (maxSalary && jobSalary > parseInt(maxSalary)) {
-  //         return false;
-  //       }
-  //     }
-  //     if (filters.location && filters.location !== job.location_job) {
-  //       return false;
-  //     }
-  //     return true;
-  //   });
-  // }
+  let filteredData = dataProducts;
+  if (dataProducts && dataProducts.length > 0) {
+    filteredData = dataProducts.filter((huydev) => {
+      console.log(huydev);
+      if (filters.categories && filters.categories !== huydev.nameCategory) {
+        return false;
+      }
+      if (filters.prices && filters.jobRoles !== huydev.level) {
+        return false;
+      }
+      if (filters.salaryRange) {
+        const [minSalary, maxSalary] = filters.salaryRange.split("-");
+        const jobSalary = parseInt(huydev.salary);
+        if (minSalary && jobSalary < parseInt(minSalary)) {
+          return false;
+        }
+        if (maxSalary && jobSalary > parseInt(maxSalary)) {
+          return false;
+        }
+      }
+      if (filters.location && filters.location !== job.location_job) {
+        return false;
+      }
+      return true;
+    });
+  }
 
   return (
     <Layout>
@@ -271,77 +270,84 @@ const FilterPage = () => {
               </div>
               <div className="mt-6 lg:col-span-4 lg:mt-0">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                  <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
-                    <div className="aspect-square rounded-xl bg-gray-100 relative">
-                      <img
-                        alt="Image"
-                        loading="lazy"
-                        decoding="async"
-                        data-nimg="fill"
-                        className="aspect-square object-cover rounded-md"
-                        sizes="100vw"
-                        srcSet="/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=640&q=75 640w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=750&q=75 750w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=828&q=75 828w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=1080&q=75 1080w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=1200&q=75 1200w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=1920&q=75 1920w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=2048&q=75 2048w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=3840&q=75 3840w"
-                        src="/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=3840&q=75"
-                        style={{
-                          position: "absolute",
-                          height: "100%",
-                          width: "100%",
-                          inset: 0,
-                          color: "transparent",
-                        }}
-                      />
-                      <div className="opacity-0 group-hover:opacity-100 transition w-full px-6 bottom-5 absolute">
-                        <div className="flex gap-x-6 justify-center">
-                          <button className="rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeWidth={2}
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="text-gray-600"
-                            >
-                              <path d="m21 21-6-6m6 6v-4.8m0 4.8h-4.8" />
-                              <path d="M3 16.2V21m0 0h4.8M3 21l6-6" />
-                              <path d="M21 7.8V3m0 0h-4.8M21 3l-6 6" />
-                              <path d="M3 7.8V3m0 0h4.8M3 3l6 6" />
-                            </svg>
-                          </button>
-                          <button className="rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeWidth={2}
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="text-gray-600"
-                            >
-                              <circle cx={8} cy={21} r={1} />
-                              <circle cx={19} cy={21} r={1} />
-                              <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
-                            </svg>
-                          </button>
+                  {filteredData?.map((item) => (
+                    <div
+                      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+                      key={item.id}
+                    >
+                      <div className="aspect-square rounded-xl bg-gray-100 relative">
+                        <img
+                          alt="Image"
+                          loading="lazy"
+                          decoding="async"
+                          data-nimg="fill"
+                          className="aspect-square object-cover rounded-md"
+                          sizes="100vw"
+                          srcSet="/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=640&q=75 640w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=750&q=75 750w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=828&q=75 828w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=1080&q=75 1080w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=1200&q=75 1200w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=1920&q=75 1920w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=2048&q=75 2048w, /_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=3840&q=75 3840w"
+                          src="/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdckypb6to%2Fimage%2Fupload%2Fv1689067600%2Fmdrvtga6qgcghgw59idx.webp&w=3840&q=75"
+                          style={{
+                            position: "absolute",
+                            height: "100%",
+                            width: "100%",
+                            inset: 0,
+                            color: "transparent",
+                          }}
+                        />
+                        <div className="opacity-0 group-hover:opacity-100 transition w-full px-6 bottom-5 absolute">
+                          <div className="flex gap-x-6 justify-center">
+                            <button className="rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={20}
+                                height={20}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="text-gray-600"
+                              >
+                                <path d="m21 21-6-6m6 6v-4.8m0 4.8h-4.8" />
+                                <path d="M3 16.2V21m0 0h4.8M3 21l6-6" />
+                                <path d="M21 7.8V3m0 0h-4.8M21 3l-6 6" />
+                                <path d="M3 7.8V3m0 0h4.8M3 3l6 6" />
+                              </svg>
+                            </button>
+                            <button className="rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={20}
+                                height={20}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="text-gray-600"
+                              >
+                                <circle cx={8} cy={21} r={1} />
+                                <circle cx={19} cy={21} r={1} />
+                                <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+                              </svg>
+                            </button>
+                          </div>
                         </div>
                       </div>
+                      <div>
+                        <p className="font-semibold text-lg">
+                          {item.nameProduct}
+                        </p>
+                        <p className="text-sm text-gray-500">
+                          {item.nameCategory}
+                        </p>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <div className="font-semibold">3.500.000&nbsp;₫</div>
+                      </div>
                     </div>
-                    <div>
-                      <p className="font-semibold text-lg">
-                        Tee Drew House Secret SS Tee Green
-                      </p>
-                      <p className="text-sm text-gray-500">Drew</p>
-                    </div>
-                    <div className="flex items-center justify-between">
-                      <div className="font-semibold">3.500.000&nbsp;₫</div>
-                    </div>
-                  </div>
+                  ))}
                 </div>
               </div>
             </div>
