a765c55c4eb86463f8d11416769dd0f3054149c7
huy <huy@gmail.com>
2023-07-27 00:20:18 +0700

    huydev

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -466,19 +466,28 @@ const HomePage = () => {
                     </React.Fragment>
                   ))}
                   {/* end products */}
-
-                  <div className="text-center pt-5">
-                    {displayedProductCount < data.length && (
-                      <button
-                        className="btn btn-danger"
-                        onClick={handleLoadMore}
-                      >
-                        {isLoading && <span>Loading...</span>} Load More
-                      </button>
-                    )}
-                  </div>
                 </div>
               )}
+              <div className="text-center pt-5">
+                {displayedProductCount < data?.length && (
+                  <button className="btn btn-danger" onClick={handleLoadMore}>
+                    {isLoading ? (
+                      <div role="status">
+                        <svg
+                          aria-hidden="true"
+                          className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        ></svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    ) : (
+                      "Load More"
+                    )}
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
