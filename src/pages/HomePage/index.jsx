cef40957fde4da992fa69defa707dcc00ecbf09c
huy <huy@gmail.com>
2023-07-27 07:36:30 +0700

    huydev

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -471,17 +471,8 @@ const HomePage = () => {
               <div className="text-center pt-5">
                 {displayedProductCount < data?.length && (
                   <button className="btn btn-danger" onClick={handleLoadMore}>
-                    {isLoading ? (
-                      <div role="status">
-                        <svg
-                          aria-hidden="true"
-                          className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                          viewBox="0 0 100 101"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        ></svg>
-                        <span className="sr-only">Loading...</span>
-                      </div>
+                    {isLoadingProduct ? (
+                        <Loading />
                     ) : (
                       "Load More"
                     )}
